6b7f1497bfc6b7649b11fdad8dbfcffb5316a4b4
kozlov211 <sayf1ck64.7@gmail.com>
2022-02-27 21:26:20 +0300

    Добавлен перебор всех состояний кодера

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,24 @@
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 import random
+
+
+def generate_numbers(alphabet: int, alphabet_length: int, prefix=None, arrays=None):
+    """
+        Генерирует все возможные варианты из алфавита N длиной M.
+        prefix - список, длины М
+    """
+    if arrays is None:
+        arrays = []
+    prefix = prefix or []
+    if alphabet_length == 0:  # крайний случай, добавляем полученную комбинацию в список arrays
+        arrays.append(prefix[:])
+        return
+    for digit in range(alphabet):  # иначе, берем цифры из алфавита N
+        prefix.append(digit)  # добавляем цифру к prefix'y
+        generate_numbers(alphabet, alphabet_length - 1, prefix, arrays)  # вызываем функцию generate_numbers
+        prefix.pop()  # удаляем последние значение в prefix, чтобы добавить другую цифру алфавита
+    return arrays
 
 
 def coding_with_speed_1_2(bits: np.array, matrix_of_polynomials: np.array, states=None):
@@ -27,6 +45,28 @@ def coding_with_speed_1_2(bits: np.array, matrix_of_polynomials: np.array, state
     return coding_sequences
 
 
+def trellis(matrix_of_polynomials):
+    number_of_registers = matrix_of_polynomials[0].size - 1  # Количество регистров
+    matrix_state = {}
+    matrix_transition = {}
+    check_transition = {}
+    for i in range(2 ** number_of_registers // 2):  # Заполнение матрицы состояний
+        matrix_state[2 * i] = (i, i + 2 ** number_of_registers // 2)
+        matrix_state[2 * i + 1] = (i, i + 2 ** number_of_registers // 2)
+        matrix_transition[2 * i] = []
+        matrix_transition[2 * i + 1] = []
+        check_transition[2 * i] = [False, False]
+        check_transition[2 * i + 1] = [False, False]
+    bits = np.array([0, 1])  # Биты для заполнения матрицы переходов
+    states = generate_numbers(2, number_of_registers)  # Генерация всех состояний
+    column = 0
+    for state in states:
+        for bit in bits:  # Заполнение матрицы переходов
+            matrix_transition[column // 2].append(coding_with_speed_1_2(np.array([bit]), matrix_of_polynomials, state))
+            column += 1
+    return matrix_state, matrix_transition, check_transition
+
+
 def add_noise_in_bits(bits: np.array, bit_error_rate: float):
     bits_with_error = np.copy(bits)
     for i in range(bits_with_error.size):
@@ -51,6 +91,82 @@ def data_training(bits_size, step, matrix_of_polynomials):
     return x_train, y_train.T
 
 
+def check_next_state(state_matrix, state, check_transition):
+    if check_transition[state][1]:
+        bit = 0
+        new_state = state_matrix[state][bit]
+        check_transition[state][bit] = True
+        return new_state
+    if check_transition[state][0]:
+        bit = 1
+        new_state = state_matrix[state][bit]
+        check_transition[state][bit] = True
+        return new_state
+    if sum(check_transition[state_matrix[state][0]]) == sum(check_transition[state_matrix[state][1]]):
+        bit = np.random.randint(2)
+        new_state = state_matrix[state][bit]
+        check_transition[state][bit] = True
+        return new_state
+    if sum(check_transition[state_matrix[state][0]]) < sum(check_transition[state_matrix[state][1]]):
+        bit = 0
+        new_state = state_matrix[state][bit]
+        check_transition[state][bit] = True
+        return new_state
+    if sum(check_transition[state_matrix[state][0]]) > sum(check_transition[state_matrix[state][1]]):
+        bit = 1
+        new_state = state_matrix[state][bit]
+        check_transition[state][bit] = True
+        return new_state
+
+
+def choice_next_state(state_matrix, state, check_transition):
+    if state == state_matrix[state][0] and not check_transition[state][0]:
+        check_transition[state][0] = True
+        return state
+    if state == state_matrix[state][1] and not check_transition[state][1]:
+        check_transition[state][1] = True
+        return state
+    return check_next_state(state_matrix, state, check_transition)
+
+
+def check_transition_zeroing(check_transition):
+    for i in range(len(check_transition)):
+        check_transition[i] = [False, False]
+
+
+def train_all_data(matrix_of_polynomials, iteration):
+    state_matrix, transition_matrix, check_transition = trellis(matrix_of_polynomials)
+    number_of_registers = matrix_of_polynomials[0].size  # Количество регистров
+    bits = np.zeros(((2 ** number_of_registers + 1) * iteration), dtype=int)
+    out_bits = np.zeros((bits.size, 2), dtype=int)
+    state = np.random.randint(2 ** (number_of_registers - 1))
+    counter = 0
+    for i in range(iteration):
+        for j in range(2 ** number_of_registers):
+            new_state = choice_next_state(state_matrix, state, check_transition)
+            if state_matrix[state][0] == new_state:
+                bits[counter] = 0
+                out_bits[counter] = transition_matrix[state][0]
+                counter += 1
+            else:
+                bits[counter] = 1
+                out_bits[counter] = transition_matrix[state][1]
+                counter += 1
+            state = new_state
+        bit = np.random.randint(2)
+        new_state = state_matrix[state][bit]
+        if state_matrix[state][0] == new_state:
+            bits[counter] = 0
+            out_bits[counter] = transition_matrix[state][0]
+            counter += 1
+        else:
+            bits[counter] = 1
+            out_bits[counter] = transition_matrix[state][1]
+            counter += 1
+        state = new_state
+        check_transition_zeroing(check_transition)
+    return out_bits
+
 def data_test_with_error(bits_size, err, step, matrix_of_polynomials):
     err_bits = step // 2 - 1
     bits_size += err_bits
@@ -66,80 +182,73 @@ def data_test_with_error(bits_size, err, step, matrix_of_polynomials):
     return x_train, y_train.T
 
 
-def choice_hyperparameters():
+def choice_hyperparameters(labels, model):
     optimizers = np.array(['SGD', 'RMSprop', 'Adam', 'RMSprop', 'Adamax', 'Adadelta', 'Nadam'])
     metrics = np.array(['Accuracy'])
     loss = np.array(['MeanSquaredError'])
     epochs = np.arange(100, 300, 15, dtype=int)
     batch_size = np.arange(18, 40, random.randint(1, 4), dtype=int)
-    layer_1 = np.arange(40, 60, random.randint(1, 4), dtype=int)
-    layer_2 = np.arange(50, 70, random.randint(1, 4), dtype=int)
-    layer_3 = np.arange(20, 30, random.randint(1, 4), dtype=int)
-    activation_1 = np.array(['relu', 'elu', 'selu', 'tanh', 'softsign'])
-    activation_2 = np.array(['relu', 'elu', 'selu', 'tanh'])
-    activation_3 = np.array(['relu', 'elu', 'selu', 'tanh', 'softsign'])
-    activation_output = np.array(['sigmoid'])
+    layers(labels, model)
     return random.choice(optimizers), random.choice(metrics), random.choice(loss), random.choice(epochs), random.choice(
-        batch_size), random.choice(layer_1), random.choice(layer_2), random.choice(layer_3), random.choice(
-        activation_1), random.choice(activation_2), random.choice(activation_3), random.choice(activation_output)
+        batch_size)
 
 
-def neural_model(hyperparameters, window, x_train, y_train, x_test, y_test):
-    optimizers = hyperparameters[0]
-    metrics = hyperparameters[1]
+def neural_model(hyperparameters, model, x_train, y_train, x_test, y_test):
+    optimizer = hyperparameters[0]
+    metric = hyperparameters[1]
     loss = hyperparameters[2]
     epochs = hyperparameters[3]
     batch_size = hyperparameters[4]
-    layer_1_size = hyperparameters[5]
-    layer_2_size = hyperparameters[6]
-    layer_3_size = hyperparameters[7]
-    activation_1 = hyperparameters[8]
-    activation_2 = hyperparameters[9]
-    activation_3 = hyperparameters[10]
-    activation_output = hyperparameters[11]
-    input = tf.keras.Input(shape=(window,), name='input')
-    layer_1 = tf.keras.layers.Dense(units=layer_1_size, activation=activation_1, name='hidden_layer_1')(input)
-    layer_2 = tf.keras.layers.Dense(units=layer_2_size, activation=activation_2, name='hidden_layer_2')(layer_1)
-    layer_3 = tf.keras.layers.Dense(units=layer_3_size, activation=activation_3, name='hidden_layer_3')(layer_2)
-    output = tf.keras.layers.Dense(units=1, activation=activation_output, name='output')(layer_3)
-    model = tf.keras.Model(inputs=input, outputs=output)
-    model.compile(optimizer=optimizers, loss=loss, metrics=metrics)
-    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
+    model.compile(optimizer=optimizer, loss=loss, metrics=metric)
+    history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
     result_test = model.evaluate(x_test, y_test)
     return result_test
 
 
-def print_hyperparameters(hyperparameters):
-    param = ['optimizers', 'metrics', 'loss', 'epochs', 'batch_size', 'layer_1_size', 'layer_2_size', 'layer_3_size',
-             'activation_1', 'activation_2', 'activation_3', 'activation_output']
-    for x, y in zip(param, hyperparameters):
-        print(x, y, sep=': ')
+def layers(labels, model):
+    layers_count = random.choice(np.arange(2, 4, 1, dtype=int))
+    layer_size = np.arange(30, 70, random.randint(1, 4), dtype=int)
+    activation = np.array(['relu', 'elu', 'selu', 'tanh', 'softsign'])
+    model.add(tf.keras.layers.Dense(random.choice(layer_size), input_dim=labels, activation=random.choice(activation)))
+    for i in range(layers_count - 1):
+        model.add(tf.keras.layers.Dense(units=random.choice(layer_size), activation=random.choice(activation)))
+    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+
+def choice_neural_model(labels, model, iterations, bits_size, matrix_of_polynomials):
+    x_train, y_train = train_all_data(matrix_of_polynomials)
+    print(x_train, y_train)
+    x_test, y_test = data_training(bits_size, labels, matrix_of_polynomials)
+    hyperparameters_result = []
+    test_result = []
+    for _ in range(iterations):
+        hyperparameters = choice_hyperparameters(labels, model)
+        print(hyperparameters)
+        hyperparameters_result.append(hyperparameters)
+        test_result.append(neural_model(hyperparameters, model, x_train, y_train, x_test, y_test))
+    max_accuracy = test_result[0][1]
+    min_loss = test_result[0][0]
+    max_param = hyperparameters_result[0]
+    for param, result in zip(hyperparameters_result, test_result):
+        if result[1] > max_accuracy and result[0] < min_loss:
+            max_accuracy = result[1]
+            max_param = param
+            min_loss = result[0]
+    print(min_loss, max_accuracy)
 
 
 # g1 = np.array([1, 1, 1])
 # g2 = np.array([1, 0, 1])
-g1 = np.array([1, 0, 1, 1, 0, 1, 1])  # 133
-g2 = np.array([1, 1, 1, 1, 0, 0, 1])  # 171
+# g1 = np.array([1, 0, 1, 1, 0, 1, 1])  # 133
+# g2 = np.array([1, 1, 1, 1, 0, 0, 1])  # 171
+g1 = np.array([1, 1, 0, 1])  # 15
+g2 = np.array([1, 1, 1, 1])  # 17
 matrix_of_polynomials = np.array([g1, g2])
-bits_size = 10000
-window = 18
-x_train, y_train = data_training(bits_size, window, matrix_of_polynomials)
-x_test, y_test = data_training(bits_size, window, matrix_of_polynomials)
-hyperparameters_result = []
-test_result = []
-
-for _ in range(10):
-    hyperparameters = choice_hyperparameters()
-    hyperparameters_result.append(hyperparameters)
-    test_result.append(neural_model(hyperparameters, window, x_train, y_train, x_test, y_test))
-max_accuracy = test_result[0][1]
-min_loss = test_result[0][0]
-max_param = hyperparameters_result[0]
-for param, result in zip(hyperparameters_result, test_result):
-    if result[1] > max_accuracy and result[0] < min_loss:
-        max_accuracy = result[1]
-        max_param = param
-        min_loss = result[0]
-
-print(min_loss, max_accuracy)
-print_hyperparameters(max_param)
+bits_size = 5000
+window = 16
+iterations = 2
+# model = tf.keras.models.Sequential()
+# state_matrix, transition_matrix, check_transition = trellis(matrix_of_polynomials)
+# print(state_matrix, transition_matrix, check_transition, sep='\n')
+print(train_all_data(matrix_of_polynomials, iterations))
+# choice_neural_model(window, model, iterations, bits_size, matrix_of_polynomials)
